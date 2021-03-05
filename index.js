dbd4b4aea693728a1f4472a312cd8668412ad9c9
IBTechRaj <raj_shk@rediffmail.com>
2021-03-05 18:45:38 +0530

    using string decoder

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ var StringDecoder = require('string_decoder').StringDecoder
 var server = http.createServer(function (req, res) {
   var parsedUrl = url.parse(req.url, true)
   var path = parsedUrl.pathname
-  var trimmedPath = path.replace(/^\/+|\/+$/g, '')
-  var parseedQueryObj = parsedUrl.query
-  var method = req.method.toLowerCase()
+  // var trimmedPath = path.replace(/^\/+|\/+$/g, '')
+  // var parseedQueryObj = parsedUrl.query
+  // var method = req.method.toLowerCase()
   var decoder = new StringDecoder('utf-8')
   var buffer = ''
   req.on('data', function (data) {
