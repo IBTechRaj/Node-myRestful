750ec49aaf6e656bf2053789377dcf5931dd7e13
IBTechRaj <raj_shk@rediffmail.com>
2021-03-05 18:17:16 +0530

    using string decoder

--- index.js
+++ index.js
@@ -6,18 +6,33 @@
 // Dependencies
 var http = require('http')
 var url = require('url')
+var StringDecoder = require('string_decoder').StringDecoder
 
 var server = http.createServer(function (req, res) {
   var parsedUrl = url.parse(req.url, true)
   var path = parsedUrl.pathname
   var trimmedPath = path.replace(/^\/+|\/+$/g, '')
-  res.end('Hello World\n')
   var parseedQueryObj = parsedUrl.query
   var method = req.method.toLowerCase()
-  console.log(
-    'Request received on path: ' + trimmedPath + ' method : ' + method + ' q ',
-    parseedQueryObj
-  )
+  var decoder = new StringDecoder('utf-8')
+  var buffer = ''
+  req.on('data', function (data) {
+    buffer += decoder.write(data)
+  })
+
+  req.on('end', function () {
+    buffer += decoder.end()
+
+    res.end('Hello World\n')
+    console.log(
+      'Request received with this payload: ' + buffer
+      //   trimmedPath +
+      //   ' method : ' +
+      //   method +
+      //   ' q ',
+      // parseedQueryObj
+    )
+  })
 })
 
 server.listen(4000, function () {
